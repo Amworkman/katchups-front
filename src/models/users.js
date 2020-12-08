95ecb1e425b4f7d1b8ae87adff861211ab8b2b84
Adam Workman <adammworkman25@gmail.com>
2020-12-08 17:55:34 -0500

    katchup card

--- src/models/users.js
+++ src/models/users.js
@@ -30,6 +30,45 @@ function selectUser(friendImg, user){
             <div class=card-bottomright>
                 <input type="button" id=card-katchup-btn name="katchup-btn" class=card-katchup-button value="katchup">
             </div>`
+
+            const katchupButton = document.getElementById("card-katchup-btn")
+            katchupButton.addEventListener("click", function(){
+                center.innerHTML = `
+                    <div id=card class=card>
+                        <div class=card-topright>                            
+                            <div id=date-picker>                            
+                            </div>
+                            <div>
+                                <form id=nav-search-location><input type="text" id=nav-search-input name=nav-search-location class=location-box placeholder="${user.location}"></form>
+                            </div>
+                        </div> 
+                        <div class=card-topleft>
+                            <div class=friend-in-card>
+                            <img class=circlecard src=${user.profile_img}>
+                                <h2>${user.name}</h2>
+                            </div>
+                        </div>
+                        <div class=card-bottomleft>                            
+                            <div>
+                            <button class="button button3"> <i class="fa fa-thumbs-down"></i></button>
+                            </div>
+                            
+                        </div>
+                        <div class=card-bottomright>
+                            <div>
+                                <button class="button button2"> <i class="fa fa-thumbs-up"></i></button>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>` 
+
+                let datepickerDefault = new MtrDatepicker({
+                    target: "date-picker",
+                });
+
+                datepickerDefault  
+            })
         }else{
             center.innerHTML +=`
             <div class=card-bottomright>
